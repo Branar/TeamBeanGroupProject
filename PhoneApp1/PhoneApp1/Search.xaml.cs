b0a7b984b654ee505569f6f842a3489e29af79db
Branar <branar3@hotmail.co.uk>
2015-04-16 15:06:24 +0100

    Version 1.1

---
+++ PhoneApp1/PhoneApp1/Search.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace PhoneApp1
+{
+    public partial class Search : PhoneApplicationPage
+    {
+        public Search()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string inputString = textBox1.Text;
+            //Search Database for matches
+            //then run matches through address in google maps
+        }
+
+    }
+}
